a37aeb8c3be8497a60e2b379f1e5fe4c34ba9f33
Josh Bradley <jbradley@arcticlcc.org>
2018-07-16 16:50:00 -0800

    Minor Updates
    - Fix deprecation form using this.route.
    - Transition to not-found when no components are present.
    - Update ember-auto-import.

--- app/pods/not-found/route.js
+++ app/pods/not-found/route.js
@@ -1,8 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  router: service(),
+
   redirect() {
-    var url = this.router.location.formatURL('/not-found');
+    var url = this.get('router').location.formatURL('/not-found');
 
     if(window.location.pathname !== url) {
       this.transitionTo('/not-found');
